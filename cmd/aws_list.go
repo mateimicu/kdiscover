cf13807672ab5d197e4811d599c3853b8dd2f440
Matei-Marius Micu <micumatei@gmail.com>
2020-05-02 02:21:30 +0300

    Add tests for table rendering

--- cmd/aws_list.go
+++ cmd/aws_list.go
@@ -11,6 +11,46 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type exportable interface {
+	IsExported(kubeconfigPath string) bool
+}
+
+func getTable(clusters []internal.Cluster) string {
+	tw := table.NewWriter()
+	tw.AppendHeader(table.Row{"Cluster Name", "Region", "Status", "Exported Locally"})
+	rows := []table.Row{}
+	for _, cls := range clusters {
+		rows = append(rows, table.Row{cls.Name, cls.Region, cls.Status, getExportedString(&cls, kubeconfigPath)})
+	}
+	tw.AppendRows(rows)
+
+	tw.AppendFooter(table.Row{"", "Number of clusters", len(clusters)})
+
+	tw.SetAutoIndex(true)
+	tw.SortBy([]table.SortBy{{Name: "Region", Mode: table.Dsc}})
+
+	tw.SetStyle(table.StyleLight)
+	tw.Style().Format.Header = text.FormatLower
+	tw.Style().Format.Footer = text.FormatLower
+	tw.Style().Options.SeparateColumns = false
+	tw.SetColumnConfigs([]table.ColumnConfig{
+		{
+			Name:        "Exported Locally",
+			Align:       text.AlignCenter,
+			AlignHeader: text.AlignCenter,
+		},
+	})
+	// render it
+	return tw.Render()
+}
+
+func getExportedString(cls exportable, kubeconfigPath string) string {
+	if cls.IsExported(kubeconfigPath) {
+		return "Yes"
+	}
+	return "No"
+}
+
 func newListCommand() *cobra.Command {
 	listCommand := &cobra.Command{
 		Use:   "list",
@@ -19,43 +59,10 @@ func newListCommand() *cobra.Command {
 			remoteEKSClusters := internal.GetEKSClusters(awsRegions)
 			log.Info(remoteEKSClusters)
 
-			tw := table.NewWriter()
-			tw.AppendHeader(table.Row{"Cluster Name", "Region", "Status", "Exported Locally"})
-			rows := []table.Row{}
-			for _, cls := range remoteEKSClusters {
-				rows = append(rows, table.Row{cls.Name, cls.Region, cls.Status, getExportedString(cls, kubeconfigPath)})
-			}
-			tw.AppendRows(rows)
-
-			tw.AppendFooter(table.Row{"", "Number of clusters", len(remoteEKSClusters)})
-
-			tw.SetAutoIndex(true)
-			tw.SortBy([]table.SortBy{{Name: "Region", Mode: table.Dsc}})
-
-			tw.SetStyle(table.StyleLight)
-			tw.Style().Format.Header = text.FormatLower
-			tw.Style().Format.Footer = text.FormatLower
-			tw.Style().Options.SeparateColumns = false
-			tw.SetColumnConfigs([]table.ColumnConfig{
-				{
-					Name:        "Exported Locally",
-					Align:       text.AlignCenter,
-					AlignHeader: text.AlignCenter,
-				},
-			})
-			// render it
-			fmt.Println(tw.Render())
-
+			fmt.Println(getTable(remoteEKSClusters))
 			return nil
 		},
 	}
 
 	return listCommand
-}
-
-func getExportedString(cls internal.Cluster, kubeconfigPath string) string {
-	if cls.IsExported(kubeconfigPath) {
-		return "Yes"
-	}
-	return "No"
 }
