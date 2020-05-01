2e9621452cd8dd2b921fadf01ac623ef2fdd220c
Matei-Marius Micu <matei.micu@mambu.com>
2020-05-01 20:36:48 +0300

    Add `aws` namespace to CLI

--- cmd/root.go
+++ cmd/root.go
@@ -12,9 +12,9 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const errorExitCode int = 1
+
 var (
-	awsPartitions  []string
-	awsRegions     []string
 	kubeconfigPath string
 	debug          bool
 	rootCmd        = &cobra.Command{
@@ -30,23 +30,6 @@ It will try to upgrade the kube-config for each cluster.`,
 			} else {
 				log.SetOutput(ioutil.Discard)
 			}
-
-			log.WithFields(log.Fields{
-				"partitions": awsPartitions,
-			}).Debug("Search regions for partitions")
-
-			awsRegions = internal.GetRegions(awsPartitions)
-
-			if len(awsRegions) == 0 {
-				log.WithFields(log.Fields{
-					"partitions": awsPartitions,
-				}).Error("Can't find regions for partitions")
-				os.Exit(1)
-			}
-
-			log.WithFields(log.Fields{
-				"regions": awsRegions,
-			}).Info("Founds regions")
 		},
 
 		Run: func(cmd *cobra.Command, args []string) {
@@ -59,23 +42,16 @@ It will try to upgrade the kube-config for each cluster.`,
 func Execute() {
 	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "set log level to debug")
 
-	rootCmd.PersistentFlags().StringSliceVar(
-		&awsPartitions,
-		"aws-partitions",
-		[]string{"aws"},
-		fmt.Sprintf("In what partitions to search for clusters. Supported %v", internal.AllowedParitions()))
-
 	rootCmd.PersistentFlags().StringVar(
 		&kubeconfigPath,
 		"kubeconfig-path",
 		internal.GetDefaultKubeconfigPath(),
 		"Path to the kubeconfig to work with")
 
-	rootCmd.AddCommand(newListCommand())
-	rootCmd.AddCommand(newUpdateCommand())
+	rootCmd.AddCommand(newAWSCommand())
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		os.Exit(errorExitCode)
 	}
 }
